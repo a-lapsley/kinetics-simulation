d6bf22ff8121ee892714d3077ed0e1d3a9e0a64d
Alex Lapsley <31190143+a-lapsley@users.noreply.github.com>
2022-12-21 11:06:09 +0000

    removed Species class for simplicity

--- reaction.py
+++ reaction.py
@@ -5,19 +5,15 @@ class Reaction:
     Class to hold all information for a specified reaction.
 
     Variables:
-        species_list:   dict    Stores each species in the reaction as a
-                                Species object in a dictionary.
+        species_list:   dict    Stores each species in the reaction as an
+                                entry in a dictionary.
         processes:      array   Stores each process in the reaction as a
                                 Process object in an array
 
     Methods:
-        add_species(self, name, species)    Adds a Species object to list
+        add_species(self, name, species)    Adds a species to list
         add_process(self, process)          Adds a Process object to list
         tick(self, delta_t)                 Proceeds reaction by time interval
-        update_all_species(self)            Updates actual concentration of 
-                                            each species
-        reset_all(self)                     Resets all concentrations to
-                                            initial values
         get_species_keys(self)              Returns list of keys for each
                                             species
         get_concs(self)                     Returns concentrations of each
@@ -32,10 +28,12 @@ class Reaction:
         self.species_list = {}
         self.processes = []
     
-    def add_species(self, species):
-        """ Adds a Species object to the species list with key 'name' """
-        name = species.get_name()
-        self.species_list[name] = species
+    def add_species(self, name, init_conc):
+        """ Adds entry to the species list with key 'name' """
+        self.species_list[name] = {
+            "conc":init_conc,
+            "delta_conc":0.0
+        }
     
     def add_process(self, process):
         """Adds a Proces object to the list of processes"""
@@ -57,33 +55,21 @@ class Reaction:
             #Calculate change in concentration for this process step
             change = k * delta_t
             for r in reactants:
-                change =  change * self.species_list[r].get_conc()
+                change =  change * self.species_list[r]["conc"]
             
             #Decrease concentration of each reactant by this amount
             for r in reactants:
-                self.species_list[r].add_to_delta_conc(-change)
+                self.species_list[r]["delta_conc"] += -change
+                
             
             #Increase concentration of each product by this amount
             for p in products:
-                self.species_list[p].add_to_delta_conc(change)
+                self.species_list[p]["delta_conc"] += change
         
-        self.update_all_species()
-        
-    def update_all_species(self):
-        """
-        For each species updates the actual concentration of the species
-        according to the total change in concentration from one step of each
-        process
-        """
         for key in self.species_list.keys():
-            self.species_list[key].update_conc()
-
-    def reset_all(self):
-        """
-        Reset concentration of all species to the initial value
-        """
-        for key in self.species_list.keys():
-            self.species_list[key].reset()
+            self.species_list[key]["conc"] += \
+                self.species_list[key]["delta_conc"]
+            self.species_list[key]["delta_conc"] = 0.0 
 
     def get_species_keys(self):
         """
@@ -97,15 +83,13 @@ class Reaction:
         """
         d = {}
         for key in self.species_list.keys():
-            d[key] = self.species_list[key].get_conc()
+            d[key] = self.species_list[key]["conc"]
         return d
             
     def log(self):
         for key in self.get_species_keys():
-            species = self.species_list[key]
-            name = species.get_name()
-            conc = species.get_conc()
-            print(name, conc)
+            conc = self.species_list[key]["conc"]
+            print(key, conc)
 
         for process in self.processes:
             reactants = process.get_reactants()
